968e98ad2b60a6206a1ee7459237720c1cea1daa
pypeaday <pypeaday@pype.dev>
2024-07-19 14:12:44 -0500

    update for working doorbell into this script if necessary

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ def get_time(endpoint, auth):
         logging.info(f"Camera time is: {(r.text).strip()}")
 
 
+def get_locales(endpoint, auth):
+    uri = endpoint + "/cgi-bin/configManager.cgi?action=getConfig&name=Locales"
+    r = requests.get(uri, auth=auth, timeout=10)
+    if r.status_code != 200:
+        logging.error(f"Status Code for {uri}: {r.status_code}")
+        logging.error(f"Reason: {r.reason}")
+
+    else:
+        logging.info(f"Camera time is: {(r.text).strip()}")
+
+
 def set_time(endpoint, auth):
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     uri = endpoint + "/cgi-bin/global.cgi?action=setCurrentTime&time=" + now
@@ -35,7 +46,16 @@ def main():
         config = yaml.safe_load(f.read())
 
     auth = HTTPDigestAuth(config["api_user"], config["api_password"])
-    for endpoint in config["endpoints"]:
+
+    # doorbell user is set to admin - I cannot change it
+    doorbell_auth = HTTPDigestAuth("admin", config["api_password"])
+
+    # don't need to sync doorbell but want it in here anyways
+    print("remove doorbell config")
+    endpoints = [x for x in config["endpoints"] if "doorbell" not in x]
+
+    for endpoint in endpoints:
+        # get_locales(endpoint, auth)
         get_time(endpoint, auth)
         set_time(endpoint, auth)
         get_time(endpoint, auth)
